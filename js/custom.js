55efe5d071969a551cd20c242153056ef809bede
VCH09 <babycasper123@gmail.com>
2017-10-18 14:51:52 +0530

    18-10-2017

--- js/custom.js
+++ js/custom.js
@@ -4,15 +4,6 @@
 $('.panel-heading').click(function(){
     $('.panel-heading').removeClass("open");
     $(this).toggleClass('open');
+    $('.panel-heading').not(this).find('i').removeClass("fa-angle-up open").addClass("fa-angle-down");
+    $(this).find('i').toggleClass("fa-angle-down fa-angle-up");
 })
-
-$(".panel-heading").mouseenter(function() {
-    $(this).find('i').toggleClass('fa-angle-down fa-angle-up');
-    $('.panel-collapse').removeClass('in');
-    $(this).closest(".panel").children('.panel-collapse').toggleClass('in');
-});
-$( ".panel").mouseleave(function() {
-    $(".panel-heading").find('i').removeClass('fa-angle-up');
-    $(".panel-heading").find('i').addClass('fa-angle-down');
-    $('.panel-collapse').removeClass('in');
-});
