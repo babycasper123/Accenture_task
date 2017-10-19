18e5f780837aacae629140f8b12b87be386615c6
VCH09 <babycasper123@gmail.com>
2017-10-19 14:11:55 +0530

    19-10-2017

--- js/custom.js
+++ js/custom.js
@@ -1,9 +1,9 @@
 
 // Left panel toggle scripts
 
-$('.panel-heading').click(function(){
-    $('.panel-heading').removeClass("open");
+$('.expand-nav').click(function(){
+    $('.expand-nav').removeClass("open");
     $(this).toggleClass('open');
-    $('.panel-heading').not(this).find('i').removeClass("fa-angle-up open").addClass("fa-angle-down");
+    $('.expand-nav').not(this).find('i').removeClass("fa-angle-up open").addClass("fa-angle-down");
     $(this).find('i').toggleClass("fa-angle-down fa-angle-up");
 })
