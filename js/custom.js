7fcc29f16f3cf6a762dcb768b68fb505cb9cd07e
Vishnu <vishnuchandran5555@gmailcom>
2017-10-23 14:13:15 +0530

    sunday

--- js/custom.js
+++ js/custom.js
@@ -7,3 +7,9 @@ $('.expand-nav').click(function(){
     $('.expand-nav').not(this).find('i').removeClass("fa-angle-up open").addClass("fa-angle-down");
     $(this).find('i').toggleClass("fa-angle-down fa-angle-up");
 })
+
+$('.contact-card').click(function(){
+				$('#myModal').modal('show');
+				}).find('.contact-btns').click(function(e){
+					return false;
+});
