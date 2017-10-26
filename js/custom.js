1d4d5944433f53412fc6dbcd978356111b44f0f4
VCH09 <babycasper123@gmail.com>
2017-10-26 18:44:00 +0530

    thursday

--- js/custom.js
+++ js/custom.js
@@ -1,6 +1,5 @@
 
 // Left panel toggle scripts
-
 $('.expand-nav').click(function(){
     $('.expand-nav').not(this).parent('.panel-heading').removeClass("open");
     $(this).parent('.panel-heading').toggleClass('open');
@@ -14,4 +13,17 @@ $('.contact-card').click(function(){
     $('#contact-card-xl').modal('show');
     }).find('.contact-btns').click(function(){
         return false;
+});
+
+// Contact history toggle scripts
+$('.contact-button').click(function(){
+    $(this).parents('.modal-content').find('.media,.modal-footer,.add-comment').hide();
+    $(this).parents('.modal-content').find('.contact-history').toggleClass('active');
+    $('.toggle-contactRecord').show();
+});
+
+$('.toggle-contactRecord').click(function(){
+    $(this).hide();
+    $('.media,.modal-footer,.add-comment').show();
+    $('.contact-history').toggleClass('active');
 });
